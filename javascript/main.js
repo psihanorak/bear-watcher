14f56f74ad40c45e624637f22f356292dbf3e1af
Phonesalo Sihanorak <psihanorak@gmail.com>
2020-05-30 14:09:32 -0500

    Added footer

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,10 @@
 document.getElementById('container-navigation').hidden = true;
+document.getElementById('container-footer').hidden = true;
 document.getElementById('learn-more').addEventListener('click', function() {
   document.getElementById('jumbotron').hidden = true;
   document.getElementById('jumbotron-bear').hidden = true;
   document.getElementById('container-navigation').hidden = false;
+  document.getElementById('container-footer').hidden = false;
 }, false);
 
 
